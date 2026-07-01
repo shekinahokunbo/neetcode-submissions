d20d5d88af2a371f8da6306179bf938e39ecc629
shekinahokunbo <so56@rice.edu>
2026-06-30 22:47:31 -0500

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+#to be an anagram, it has to have same numbers of letters, and also
+#be the same string just reversed. one way might be to reverse the second
+#string and check if this new list is equal to the old list 
+
+        # new_list = list(s)
+        # betterlist = new_list.reverse
+
+        # if new_list == list(t):
+        #     return True
+        # return False
+
+# one mistake I made here was that I didnt see that the orders can be different
+# convert to list, sort them, if they are equal, then true
+
+        new_list = set(s)
+        better_list = list(new_list)
+        list2 = set(t)
+        final_list = list(list2)
+        if sorted(better_list) == sorted(final_list) and sorted(s) == sorted(t):
+            return True
+        return False
